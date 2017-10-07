8a918aa4b01410b309cbed8b1f43483ca476c5e6
Befkadu Degefa <befkassefa@gmail.com>
2017-10-07 03:55:53 +0200

    adding the mobile app

---
+++ car_wash_app/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import RootComponent from './components/rootComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+
+        <RootComponent />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ car_wash_app/components/rootComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class RootComponent extends Component {
+
+
+  render() {
+    return (
+<View>
+
+        <Text>
+
+
+
+
+          
+          You just use native components like 'View' and 'Text',
+           instead of web components like 'div' and 'span'.
+        </Text>
+</View>
+);
+  }
+
+}
